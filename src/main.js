0dbaec276d03c6c5f8073ee2d7672834c6926aa4
Will Tsai <wntsai@ucsc.edu>
2023-11-01 17:56:45 -0700

    Asset init

---
+++ src/main.js
@@ -0,0 +1,7 @@
+/*
+
+Background Artwork was created by myself and group for CMPM 80K during Summer Session 1 
+Background Music was created by @Jayu on Youtube. Utilized with full usage permissions.
+
+*/
+
